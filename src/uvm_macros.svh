379b64b4b5fa007c83e8da501fb2c5029586641d
stornado <ryan@zxytech.com>
2023-10-18 22:27:16 +0800

    UVM 1.1a Reference Implementation

--- src/uvm_macros.svh
+++ src/uvm_macros.svh
@@ -28,11 +28,11 @@
 `define _protected protected   
 `define UVM_USE_FPC
 `define UVM_USE_P_FORMAT
-`define UVM_USE_FILE_LINE
-`define UVM_DA_TO_QUEUE(Q, DA) Q=DA;
+//`define UVM_USE_FILE_LINE
+`define UVM_DA_TO_QUEUE(Q,DA) Q=DA;
 `define _local local
 `define uvm_delay(TIME) #(TIME);
-
+`define UVM_USE_TYPENAME
 //
 // Any vendor specific defines go here.
 //
@@ -47,31 +47,42 @@
 `endif
 
 `ifdef QUESTA
-//`define UVM_USE_BITSTREAM_OPS
+`define UVM_EXTRA_TYPENAME_ARG
 `endif
 
 `ifdef INCA
-`ifndef INCA_PROTECTED_CTOR
-`undef _protected
-`define _protected
+  `ifndef INCA_PROTECTED_CTOR
+    `undef _protected
+    `define _protected 
+  `endif
+  `ifndef INCA_LOCAL_CTOR
+    `undef _local
+    `define _local 
+  `endif
+  `ifndef INCA_UVM_USE_P_FORMAT
+    `undef  UVM_USE_P_FORMAT
+  `endif
+//  `ifndef INCA_UVM_USE_FILE_LINE
+//    `undef  UVM_USE_FILE_LINE
+//  `endif
+  `ifndef INCA_UVM_USE_FPC
+    `undef UVM_USE_FPC
+  `endif
+  `define UVM_USE_PROCESS_CONTAINER
+  `undef  UVM_DA_TO_QUEUE
+  `define UVM_DA_TO_QUEUE(Q,DA)  foreach (DA[idx]) Q.push_back(DA[idx]);
+  `ifndef INCA_USE_TYPENAME
+    `undef UVM_USE_TYPENAME
+  `endif
 `endif
-`ifndef INCA_LOCAL_CTOR
-`undef _local
-`define _local
+
+//
+// Deprecation Control Macros
+//
+`ifdef UVM_NO_DEPRECATED
+  `define UVM_OBJECT_MUST_HAVE_CONSTRUCTOR
 `endif
-`ifndef INCA_UVM_USE_P_FORMAT
-`undef UVM_USE_P_FORMAT
-`endif
-`ifndef INCA_UVM_USE_FILE_LINE
-`undef UVM_USE_FILE_LINE
-`endif
-`ifndef INCA_UVM_USE_FPC
-`undef UVM_USE_FPC
-`endif
-`define UVM_USE_PROCESS_CONTAINER
-`undef UVM_DA_TO_QUEUE
-`define UVM_DA_TO_QUEUE(Q, DA) foreach (DA[idx]) Q.push_back(DA[idx]);
-`endif
+
 
 `include "macros/uvm_version_defines.svh"
 `include "macros/uvm_message_defines.svh"
